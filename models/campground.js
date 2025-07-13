7b83ac84dc0bdbd106df8fd4e9d3843638523d20
ototosiki <otsuka0125g@gmail.com>
2025-07-13 21:57:15 +0900

    レビュー機能を追加

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Review = require('./review');
+const { Schema } = mongoose;
+
 
 
 const campgroundSchema = new Schema({
@@ -8,6 +10,22 @@ const campgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref:'Review'
+        }
+    ]
 });
+
+campgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+         await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports =  mongoose.model('Campground', campgroundSchema);
